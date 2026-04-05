c6d5d25a1934f15941dc8bf3cef94da00c39b0ff
Alexandr <traxex864@gmail.com>
2026-04-05 14:29:21 +0700

    fix: remove unused Link import

--- frontend/src/pages/ResetPasswordPage.tsx
+++ frontend/src/pages/ResetPasswordPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, useSearchParams, Link } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import api from '../api/axios';
 import Button from '../components/Button';
